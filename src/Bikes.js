25a65feaf4de7af297cbd97ae276bc2f667c9e51
Simon Dell <simon@expressionworks.co.uk>
2017-02-07 14:55:13 +0000

    Fetches bike list and shows initial results

--- src/Bikes.js
+++ src/Bikes.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 
+const BIKE_URL = 'https://jujhar.com/bikes.json';
+
 class Bikes extends Component {
+
+	componentDidMount() {
+		fetch(BIKE_URL)
+		.then( response => {
+			console.log( response );
+			if( response.ok ) return response.json();
+			else throw 'Could not reach data URI';
+		})
+		.then( body => body.items )
+		.then( bikes => { this.setState( { bikes: bikes } ) } );
+	}
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			bikes: []
+		};
+	}
+
+	getBikeList() {
+		return ( this.state.bikes.map( bike => <li>{ bike.name }</li> ) );
+	}
+
 	render() {
 		return (
-			<p>There are bikes</p>
+			<ol>{ this.getBikeList() }</ol>
 		);
 	}
 }
